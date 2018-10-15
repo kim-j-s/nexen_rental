8a728565669e0a46483feb32783af482d86083ab
kjs <kogwing@gmail.com>
2018-10-15 10:11:37 +0900

    웹 초기

---
+++ web/static/js/ui.js
@@ -0,0 +1,162 @@
+$(function(){
+	
+	/* ==============================
+	 * common
+	 * ============================== */
+	
+	selectMake();
+	selectMakeUI();
+
+	//datepicker
+	if($('.datepicker').size() > 0){
+		$( '.datepicker' ).datepicker({
+			closeText: '닫기',
+			prevText: '이전 달',
+			nextText: '다음 달',
+			currentText: '오늘',			
+			monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+            monthNamesShort: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+			dayNames: ['일','월','화','수','목','금','토'],
+            dayNamesShort: ['일','월','화','수','목','금','토'],
+            dayNamesMin: ['일','월','화','수','목','금','토'],
+			dateFormat: 'yy.mm.dd',
+			showMonthAfterYear: true,
+			changeMonth: true,
+      		changeYear: true,
+      		yearSuffix: '년',
+			showOn: 'button',
+			buttonText: '기간조회'
+		});
+	}
+
+	//swiper
+	if($('.swiper').size() != 0){
+		 $('.swiper').slick({
+		  dots: true,
+		  arrows:true,
+		  roof:false,
+		  infinite: false,
+		  speed: 1300
+		});
+	}
+
+	if($('.loding-act').size() > 0){
+		//http://kottenator.github.io/jquery-circle-progress/
+		var lodingActVal = parseInt($('.loding-act .loding-txt span').text());
+		$('.loding-act').circleProgress({
+		  value: lodingActVal/100, //변수값
+		  startAngle:-Math.PI / 2, //스타트 지점설정
+		  fill : { color:"red"}, //색상값
+		  emptyFill:'silver', //뒷 색상값
+		  size:120 // 전체 사이즈 
+		}).on('circle-animation-progress', function(event, progress) {
+		  $(this).find('.loding-txt').html('진도율 <br /><span>' + parseInt(lodingActVal * progress) + '%</span>');
+		});
+	}
+	
+	/* ==============================
+	 * gnb 
+	 * ============================== */
+
+
+
+	/* ==============================
+	 * main 
+	 * ============================== */
+
+
+	/* ==============================
+	 * content 
+	 * ============================== */
+	 
+});
+
+/* parallax scrolling motion */
+scrollAnimation();
+function scrollAnimation(){
+	$(window).load(function(){
+		var $elements = $( '*[data-animation]' );
+		var h = $(window).height()
+		$elements.each( function( i, el ) {
+			var $el = $( el ),
+			    animationClass = $el.data('animation'),
+			    $delay = $el.data('delay'),
+			    $duration = $el.data('duration');
+			
+			if($delay>0){
+				$el.css({
+					'-webkit-animation-delay':$delay+'s',
+					'animation-delay':$delay+'s'
+				})
+			}
+			if($duration>0){
+				$el.css({
+					'-webkit-animation-duration':$duration+'s',
+					'animation-duration':$duration+'s'
+				})
+			}
+
+			var t = $el.offset().top;
+			if(t > h){
+				$el.addClass('wait-animation');
+			}
+			$el.addClass('animated '+animationClass);
+
+			$el.waypoint(function(){
+				$el.removeClass('wait-animation');
+			}, { offset: '100%', triggerOnce: true });
+		});
+	});
+}
+
+
+/* form select */
+function selectMake(){
+	$("select.selectBox").each(function(){
+		if($(this).parents('pre').length < 1){
+			var classes = $(this).attr("class"),
+				id      = $(this).attr("id"),
+				name    = $(this).attr("name");
+				style	= $(this).attr('style');
+				
+			if($(this).is(':visible')){
+				var template  = '<div class="' + classes + '" style="' +  style + '">';
+					template += '<a href="#" class="ui-select-trigger">' + $(this).find(':selected').text() + '</a>';
+					template += '<ul class="ui-select-options">';
+					$(this).find("option").each(function(){
+						template += '<li><a href="#" class="ui-select-option" data-value="' + $(this).attr("value") + '">' + $(this).html() + '</a></li>';
+					});
+					template += '</ul></div>';
+			  
+				$(this).wrap('<div class="ui-select-wrapper"></div>');
+				$(this).hide().after(template);
+			}
+		}
+	});
+}
+
+function selectMakeUI(){
+	$(document).on("hover",".ui-select-option:first-of-type",function(){
+	  $(this).closest(".ui-select-options").addClass("ui-select-option-hover");
+	}, function(){
+	  $(this).closest(".ui-select-options").removeClass("ui-select-option-hover");
+	});
+	$(document).on("click",".ui-select-trigger", function(e){
+		$('.ui-select-options').not($(this).next()).hide();
+		$(this).next().show();
+		return false;
+	});
+	$(document).click(function(e){
+		$('.ui-select-options').hide();
+	});
+	$(document).on("click",".ui-select-option", function(e){
+	  var $val= $(this).data("value"),
+		  $select = $(this).closest(".ui-select-wrapper").find("select");
+	  
+	  $select.val($val);
+	  $(this).addClass("selection").parent().siblings().find(".ui-select-option").removeClass("selection");
+	  $(this).closest('.ui-select-options').hide().siblings(".ui-select-trigger").text($(this).text());
+	  return false;
+	});
+}
+
