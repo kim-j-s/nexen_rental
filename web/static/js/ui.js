75e72cb82b90ce4346f24f5770967255a82f4943
jeongaeng <jeonkm1112@gmail.com>
2019-01-09 20:29:18 +0900

    웹 수정 - 이미지크게보기 딤레이어샘플

--- web/static/js/ui.js
+++ web/static/js/ui.js
@@ -1277,3 +1277,38 @@ function uiForm() {
 		$('[for="'+labelFor+'"]').addClass('on');
 	});
 }
+
+// 이미지크게보기 딤레이어처리
+function layer_popup(el){
+
+	var $el = $(el);        //레이어의 id를 $el 변수에 저장
+	var isDim = $el.prev().hasClass('dimBg');   //dimmed 레이어를 감지하기 위한 boolean 변수
+
+	isDim ? $('.dimLayer').fadeIn() : $el.fadeIn();
+
+	var $elWidth = ~~($el.outerWidth()),
+		$elHeight = ~~($el.outerHeight()),
+		docWidth = $(document).width(),
+		docHeight = $(document).height();
+
+	// 화면의 중앙에 레이어를 띄운다.
+	if ($elHeight < docHeight || $elWidth < docWidth) {
+		$el.css({
+			marginTop: -$elHeight /2,
+			marginLeft: -$elWidth/2
+		})
+	} else {
+		$el.css({top: 0, left: 0});
+	}
+
+	$el.find('a.layerClose').click(function(){
+		isDim ? $('.dimLayer').fadeOut() : $el.fadeOut(); // 닫기 버튼을 클릭하면 레이어가 닫힌다.
+		return false;
+	});
+
+	$('.dimLayer .dimBg').click(function(){
+		$('.dimLayer').fadeOut();
+		return false;
+	});
+
+}
